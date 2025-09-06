962551c5ed7e3e3abb6b444c41b02908579034c7
maimunarashid <rashidromana05@gmail.com>
2025-09-06 13:44:48 +0600

    second commit - JS heart icon click counting

---
+++ index.js
@@ -0,0 +1,65 @@
+// Heart Icons- Clicking on the heart icon of any card will increase the count in the Navbar
+    document.getElementById("heart-icon1").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon2").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon3").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon4").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon5").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon6").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon7").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon8").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    document.getElementById("heart-icon9").addEventListener('click', function(e){
+        e.preventDefault();
+        let heartCount = parseInt(document.getElementById("heart-count").innerText);
+        heartCount = heartCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    })
+
+    
